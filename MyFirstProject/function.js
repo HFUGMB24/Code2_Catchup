d0fe6b7ee9f7bfbd66a988b33ecf0d6108fac724
Alexander Scheurer <mail@aspepex.net>
2024-10-24 14:15:55 +0200

    function stuff

---
+++ MyFirstProject/function.js
@@ -0,0 +1,9 @@
+"use strict";
+let globalNumberOne = 10;
+let globalNumberTwo = 143;
+function add(_functionNumberOne, _functionNumberTwo) {
+    return _functionNumberOne + _functionNumberTwo;
+}
+let result = add(globalNumberTwo, globalNumberOne);
+console.log(result);
+//# sourceMappingURL=function.js.map
---
+++ MyFirstProject/function.ts
@@ -0,0 +1,11 @@
+
+let globalNumberOne: number = 10
+let globalNumberTwo: number = 143
+
+function add(_functionNumberOne: number, _functionNumberTwo: number): number {
+  return _functionNumberOne + _functionNumberTwo
+}
+
+let result = add(globalNumberTwo, globalNumberOne)
+
+console.log(result)
