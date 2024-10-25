a610136a8c655bc83f7a50a5f2e55850db53c344
Alexander Scheurer <mail@aspepex.net>
2024-10-25 09:52:36 +0200

    Interfaces final

--- 02_Interfaces/interface.js
+++ 02_Interfaces/interface.js
@@ -1,14 +1,38 @@
 "use strict";
-let authorsDatabase = [];
-authorsDatabase.push("Douglas Adams");
-authorsDatabase.push("Alexander Scheurer");
-authorsDatabase.push("Stephen King");
-let tempAuthors = [];
-tempAuthors.push(authorsDatabase[0]);
+let authorsDatabase = [
+    "Douglas Adams",
+    "Alexander Scheurer",
+    "Stephen King"
+];
 let book1 = {
     title: "Hitchhiker",
     pages: 1287,
-    authors: tempAuthors
+    authors: [authorsDatabase[0]],
+    chapters: ["Erstes Kapitel", "Chapert Two"]
 };
-console.log(book1);
+let book2 = {
+    title: "It",
+    pages: 706,
+    authors: [authorsDatabase[2]],
+    chapters: ["Kapitel Zehn", "Kapitel Blub"]
+};
+let bookcase = { room: "Shower", books: [book1, book2] };
+//               2              1
+function read(_book, _chapter) {
+    let bookIndex = _book - 1;
+    let chapterIndex = _chapter - 1;
+    return bookcase.books[bookIndex].chapters[chapterIndex];
+}
+//                                 "It"
+function howManyPagesDoesBookHave(_title) {
+    for (const book of bookcase.books) {
+        if (book.title == _title) {
+            return book.pages;
+        }
+    }
+    return -1;
+}
+//                                   _book  _chapter
+let chapterIWannaRead = read(2, 1);
+console.log(howManyPagesDoesBookHave("It"));
 //# sourceMappingURL=interface.js.map
--- 02_Interfaces/interface.ts
+++ 02_Interfaces/interface.ts
@@ -1,22 +1,62 @@
-
 interface Book {
   title: string;
   pages: number;
   authors: string[];
+  chapters: string[]
 }
 
-let authorsDatabase: string[] = []
-authorsDatabase.push("Douglas Adams")
-authorsDatabase.push("Alexander Scheurer")
-authorsDatabase.push("Stephen King")
+interface Bookcase {
+  room: string
+  books: Book[]
+}
 
-let tempAuthors: string[] = []
-tempAuthors.push(authorsDatabase[0])
+let authorsDatabase: string[] = [
+  "Douglas Adams",
+  "Alexander Scheurer",
+  "Stephen King"
+]
 
 let book1: Book = {
   title: "Hitchhiker",
   pages: 1287,
-  authors: tempAuthors
+  authors: [authorsDatabase[0]],
+  chapters: ["Erstes Kapitel", "Chapert Two"]
 }
 
-console.log(book1)
+let book2: Book = {
+  title: "It",
+  pages: 706,
+  authors: [authorsDatabase[2]],
+  chapters: ["Kapitel Zehn", "Kapitel Blub"]
+}
+
+let bookcase: Bookcase = { room: "Shower", books: [book1, book2] }
+
+//               2              1
+function read(_book: number, _chapter: number): string {
+
+  let bookIndex: number = _book - 1
+  let chapterIndex: number = _chapter - 1
+
+  return bookcase.books[bookIndex].chapters[chapterIndex]
+}
+
+//                                 "It"
+function howManyPagesDoesBookHave(_title: string): number {
+
+  for (const book of bookcase.books) {
+
+    if (book.title == _title) {
+      return book.pages
+    }
+
+  }
+
+  return -1
+}
+
+//                               _book, _chapter
+let chapterIWannaRead: string = read(2, 1)
+
+console.log(howManyPagesDoesBookHave("It"))
+
