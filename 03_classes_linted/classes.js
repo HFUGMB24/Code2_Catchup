3390e87f60cfef575d959a76b48bbf35efbc339b
Alexander Scheurer <mail@aspepex.net>
2024-10-28 11:41:03 +0100

    never recompiled these

--- 03_classes_linted/classes.js
+++ 03_classes_linted/classes.js
@@ -1,5 +1,5 @@
 "use strict";
-let authorsDatabase = [
+const authorsDatabase = [
     "Douglas Adams",
     "Alexander Scheurer",
     "Stephen King"
@@ -12,8 +12,8 @@ class Book {
         this.chapters = _chapters;
     }
 }
-let book1 = new Book("Hitchhiker", [authorsDatabase[0]], 1287, ["K1", "K2"]);
-let book2 = new Book("It", [authorsDatabase[2]], 123, ["It1", "It2"]);
+const book1 = new Book("Hitchhiker", [authorsDatabase[0]], 1287, ["K1", "K2"]);
+const book2 = new Book("It", [authorsDatabase[2]], 123, ["It1", "It2"]);
 class Bookcase {
     constructor(_room, _books) {
         this.room = _room;
@@ -31,37 +31,7 @@ class Bookcase {
         return;
     }
 }
-let bookcase = new Bookcase("Shower", [book1, book2]);
+const bookcase = new Bookcase("Shower", [book1, book2]);
 bookcase.addBook(new Book("Doofe Thesis", [authorsDatabase[1]], 123, ["Blöd1", "Blöd22"]));
-console.log(bookcase.searchBook("Bible"));
-//               2              1
-function read(_book, _chapter) {
-    let bookIndex = _book - 1;
-    let chapterIndex = _chapter - 1;
-    return bookcase.books[bookIndex].chapters[chapterIndex];
-}
-//                                 "It"
-function howManyPagesDoesBookHave(_title) {
-    // for (let i = 0; i < bookcase.books.length; i++) {
-    //   const book = bookcase.books[i];
-    //   if (book.title == _title) {
-    //     return book.pages
-    //   }
-    // }
-    // for (const i in bookcase.books) {
-    //   const book = bookcase.books[i];
-    //   if (book.title == _title) {
-    //     return book.pages
-    //   }
-    // }
-    for (const book of bookcase.books) {
-        if (book.title == _title) {
-            return book.pages;
-        }
-    }
-    return -1;
-}
-//                                   _book  _chapter
-let chapterIWannaRead = read(2, 1);
-//console.log(howManyPagesDoesBookHave("It"))
+console.log(bookcase.searchBook("It"));
 //# sourceMappingURL=classes.js.map
